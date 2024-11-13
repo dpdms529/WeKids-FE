919f6e7c4a45eea529eea85c7b0f47c81190ed06
jabin1221 <koreanjb1221@gmail.com>
2024-11-13 16:01:38 +0900

    [feat/#37]: 약관동의 뷰

--- src/app/(login)/self/page.jsx
+++ src/app/(login)/self/page.jsx
@@ -21,7 +21,21 @@ export default function Page() {
                 <h2>휴대폰 인증</h2>
                 <input type="button" value="휴대폰 인증 완료" disabled className="rounded-lg w-full bg-slate-400 border-2 p-2 mt-1 text-white"></input>
             </div>
-            
+            <div className="px-10 py-5">
+                <h2>약관동의</h2>
+                <div className="border-black border-2 h-68 rounded-lg">
+                    <Assign />
+                    <Assign />
+                    <Assign />
+                    <Assign />
+                </div>
+                
+            </div>
+            <div className="px-10 py-5">
+                <CustomButton rounded = "true" className="w-full" >
+                    {"다음"}
+                </CustomButton>
+            </div>
             
         </div>
     );
