eb4e25ebe3d64c3669bf277852fe2e8d327390f2
yoon <cbj0010@naver.com>
2024-11-14 01:18:30 +0900

    [feat/#29]: popup 사용방법 추가

--- src/app/ui/button/page.jsx
+++ src/app/ui/button/page.jsx
@@ -6,11 +6,12 @@ import { useState } from "react";
 
 const Page = () => {
   const [placeholderValue, setPlaceholderValue] = useState("값을 입력하세요");
+  const [isPopupOpen, setIsPopupOpen] = useState(true); // 팝업 열림/닫힘 상태 관리
   const handlePlaceholderChange = (newPlaceholder) => {
     setPlaceholderValue(newPlaceholder);
   };
   const handleButtonClick = () => {
-    // 버튼 클릭 시 실행되는 함수 구현
+    setIsPopupOpen(false);
   };
   return (
     <>
@@ -24,13 +25,16 @@ const Page = () => {
       </div>
 
       <div>
-        <PopupMessage
-          title="메시지 등록 완료"
-          message="메시지 등록이 완료되었습니다."
-          buttonText="확인"
-          onButtonClick={handleButtonClick}
-          height="208px"
-        />
+        {/* 팝업이 열려 있으면 PopupMessage 컴포넌트 렌더링 */}
+        {isPopupOpen && (
+          <PopupMessage
+            title="팝업 제목"
+            message="이것은 팝업 메시지입니다."
+            buttonText="확인"
+            onButtonClick={handleButtonClick} // 확인 버튼 클릭 시 실행할 함수
+            onClose={handleButtonClick} // X 버튼 클릭 시 팝업을 닫는 함수
+          />
+        )}
       </div>
 
       {/* large */}
