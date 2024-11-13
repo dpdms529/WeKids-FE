1ccdf9da6055e4a085a68abe39c88a0c52631b0b
jabin1221 <koreanjb1221@gmail.com>
2024-11-13 16:20:31 +0900

    [feat/#37]: 휴대폰 인증 버튼 수정

--- src/app/(login)/self/page.jsx
+++ src/app/(login)/self/page.jsx
@@ -1,11 +1,13 @@
+'use client'
+
 
 import Assign from "@/src/ui/Components/atoms/Assign";
 import CustomButton from "@/src/ui/Components/atoms/CustomButton";
-import React from "react";
+import React, {useState} from "react";
 
 export default function Page() {
 
-    
+    const [phoneChecked, setphoneChecked] = useState(true); // 추후에 zustand로 설정 예정
 
     return (
         <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
@@ -19,7 +21,10 @@ export default function Page() {
             </div>
             <div className="px-10 py-5">
                 <h2>휴대폰 인증</h2>
-                <input type="button" value="휴대폰 인증 완료" disabled className="rounded-lg w-full bg-slate-400 border-2 p-2 mt-1 text-white"></input>
+                <CustomButton rounded = "true" className={`w-full ${phoneChecked ? "" : "bg-black/40 hover:bg-black/40"}`} onClick={() => setphoneChecked(!phoneChecked)}>
+                     휴대폰 인증하기
+                </CustomButton>
+                
             </div>
             <div className="px-10 py-5">
                 <h2>약관동의</h2>
