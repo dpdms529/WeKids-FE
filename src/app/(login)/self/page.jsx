9c930c7167a922d1242262681cda16effb43dc95
jabin1221 <koreanjb1221@gmail.com>
2024-11-13 15:58:13 +0900

    [feat/#37]: 개인정보 입력 초기 뷰

---
+++ src/app/(login)/self/page.jsx
@@ -0,0 +1,28 @@
+
+import Assign from "@/src/ui/Components/atoms/Assign";
+import CustomButton from "@/src/ui/Components/atoms/CustomButton";
+import React from "react";
+
+export default function Page() {
+
+    
+
+    return (
+        <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
+            <div className="px-10 py-5">
+                <h2>이메일</h2>
+                <input className="rounded-lg w-full border-black border-2 p-2 mt-1"></input>
+            </div>
+            <div className="px-10 py-5">
+                <h2>이름</h2>
+                <input className="rounded-lg w-full border-black border-2 p-2 mt-1"></input>
+            </div>
+            <div className="px-10 py-5">
+                <h2>휴대폰 인증</h2>
+                <input type="button" value="휴대폰 인증 완료" disabled className="rounded-lg w-full bg-slate-400 border-2 p-2 mt-1 text-white"></input>
+            </div>
+            
+            
+        </div>
+    );
+}
