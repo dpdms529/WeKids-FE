491b41b3965e7ffa4c796e578fa1e7fc948d86d5
jabin1221 <koreanjb1221@gmail.com>
2024-11-18 18:40:23 +0900

    [feat/#36]: 기본 적인 뷰 구현

--- src/app/(login)/select/page.jsx
+++ src/app/(login)/select/page.jsx
@@ -11,8 +11,35 @@ export default function Page() {
             <div className="my-24">
                 <a className="text-B-28">부모인가요? 자녀인가요?</a>
             </div>
-            
-            <div className=" fixed bottom-5">
+            <div className="w-full">
+                <div className="rounded-lg border-black w-full h-20 border-2 my-7 shadow-xl cursor-pointer">
+                    <div className="flex flex-row">
+                        <div className="w-1/3 h-20 flex items-center justify-center">
+                            <div className="rounded-full w-10 h-10 bg-neutral-400">
+                                <CheckIcon className="w-10 h-10 text-black"/>
+                            </div>
+                        </div>  
+                        <div className="w-2/3 h-20 flex flex-col justify-center">
+                            <p className="text-R-12">자녀에게 용돈을 줄거에요.</p>
+                            <p className="text-R-20 w-">부모입니다.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="rounded-lg border-black w-full h-20 border-2 my-3 shadow-xl cursor-pointer">
+                    <div className="flex flex-row">
+                        <div className="w-1/3 h-20 flex items-center justify-center">
+                            <div className="rounded-full w-10 h-10 bg-neutral-400">
+                                <CheckIcon className="w-10 h-10 text-black"/>
+                            </div>
+                        </div>  
+                        <div className="w-2/3 h-20 flex flex-col justify-center">
+                            <p className="text-R-12">용돈을 받고 금융도 배울거에요.</p>
+                            <p className="text-R-20">자녀입니다.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fixed bottom-5">
                 <ConfirmButton />
             </div>
         </div>
