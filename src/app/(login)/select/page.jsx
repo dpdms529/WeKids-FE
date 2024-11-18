f49ba58293a7a5a71a34d4891fd294bec9dadd08
jabin1221 <koreanjb1221@gmail.com>
2024-11-18 19:06:42 +0900

    [feat/#36]: 체크박스 상태 구현

--- src/app/(login)/select/page.jsx
+++ src/app/(login)/select/page.jsx
@@ -1,22 +1,34 @@
+'use client';
+
 import ConfirmButton from "@/src/ui/Components/transaction/detail/ConfirmButton";
-import React from "react";
+import React, {useState} from "react";
 import { CheckIcon } from "@radix-ui/react-icons";
 
 
 
 export default function Page() {
 
+    const [isParentClicked, setParentClicked] = useState(false);
+    const [isChildClicked, setChildClicked] = useState(false);
+
+
+
+
+
     return(
         <div className="flex flex-col h-screen bg-white overflow-y-auto w-[393px] items-center px-10"> 
-            <div className="my-24">
+            <div className="my-20 py-2">
                 <a className="text-B-28">부모인가요? 자녀인가요?</a>
             </div>
             <div className="w-full">
-                <div className="rounded-lg border-black w-full h-20 border-2 my-7 shadow-xl cursor-pointer">
-                    <div className="flex flex-row">
+                <div className="rounded-xl border-neutral-400 w-full h-20 border-2 my-7 shadow-xl cursor-pointer">
+                    <div className="flex flex-row" onClick={() => {
+                                setParentClicked(true);
+                                setChildClicked(false);
+                            }}>
                         <div className="w-1/3 h-20 flex items-center justify-center">
-                            <div className="rounded-full w-10 h-10 bg-neutral-400">
-                                <CheckIcon className="w-10 h-10 text-black"/>
+                            <div className="rounded-full w-10 h-10 bg-neutral-400" >
+                                <CheckIcon className={`w-10 h-10 ${isParentClicked ? "text-black" : "text-white"} `}/>
                             </div>
                         </div>  
                         <div className="w-2/3 h-20 flex flex-col justify-center">
@@ -25,11 +37,14 @@ export default function Page() {
                         </div>
                     </div>
                 </div>
-                <div className="rounded-lg border-black w-full h-20 border-2 my-3 shadow-xl cursor-pointer">
-                    <div className="flex flex-row">
+                <div className="rounded-xl border-neutral-400 w-full h-20 border-2 my-3 shadow-xl cursor-pointer">
+                    <div className="flex flex-row" onClick={() => {
+                                setParentClicked(false);
+                                setChildClicked(true);
+                            }}>
                         <div className="w-1/3 h-20 flex items-center justify-center">
                             <div className="rounded-full w-10 h-10 bg-neutral-400">
-                                <CheckIcon className="w-10 h-10 text-black"/>
+                                <CheckIcon className={`w-10 h-10 ${isChildClicked ? "text-black" : "text-white"} `}/>
                             </div>
                         </div>  
                         <div className="w-2/3 h-20 flex flex-col justify-center">
