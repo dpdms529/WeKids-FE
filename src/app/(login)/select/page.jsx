7793c8ac5b603b9f5c0453e8458a8361fe2a61cf
jabin1221 <koreanjb1221@gmail.com>
2024-11-18 20:42:12 +0900

    [feat/#36]: 컴포넌트 분리

--- src/app/(login)/select/page.jsx
+++ src/app/(login)/select/page.jsx
@@ -2,7 +2,7 @@
 
 import ConfirmButton from "@/src/ui/Components/transaction/detail/ConfirmButton";
 import React, {useState} from "react";
-import { CheckIcon } from "@radix-ui/react-icons";
+import ParentChildSelector from "@/src/ui/Components/atoms/signup/ParentChildSelector";
 
 
 
@@ -18,38 +18,20 @@ export default function Page() {
                 <a className="text-B-28">부모인가요? 자녀인가요?</a>
             </div>
             <div className="w-full">
-                <div className={`rounded-xl ${isParentClicked ? "border-black" : "border-neutral-400"} w-full h-20 border-2 my-7 shadow-xl cursor-pointer`}>
-                    <div className="flex flex-row" onClick={() => {
-                                setParentClicked(true);
-                                setChildClicked(false);
-                            }}>
-                        <div className="w-1/3 h-20 flex items-center justify-center">
-                            <div className={`rounded-full w-10 h-10 ${isParentClicked ? "bg-main01" : "bg-neutral-400"}`} >
-                                <CheckIcon className={`w-10 h-10 ${isParentClicked ? "text-black" : "text-white"} `}/>
-                            </div>
-                        </div>  
-                        <div className={`w-2/3 h-20 flex flex-col gap-2 justify-center ${isParentClicked ? "text-black" : "text-neutral-400"}`}>
-                            <p className="text-R-12">자녀에게 용돈을 줄거에요.</p>
-                            <p className="text-R-20 w-">부모입니다.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className={`rounded-xl ${isChildClicked ? "border-black" : "border-neutral-400"} w-full h-20 border-2 my-3 shadow-xl cursor-pointer`}>
-                    <div className="flex flex-row" onClick={() => {
-                                setParentClicked(false);
-                                setChildClicked(true);
-                            }}>
-                        <div className="w-1/3 h-20 flex items-center justify-center">
-                            <div className={`rounded-full w-10 h-10 ${isChildClicked ? "bg-main01" : "bg-neutral-400"}`}>
-                                <CheckIcon className={`w-10 h-10 ${isChildClicked ? "text-black" : "text-white"} `}/>
-                            </div>
-                        </div>  
-                        <div className={`w-2/3 h-20 flex flex-col gap-2 justify-center ${isChildClicked ? "text-black" : "text-neutral-400"}`}>
-                            <p className="text-R-12">용돈을 받고 금융도 배울거에요.</p>
-                            <p className="text-R-20">자녀입니다.</p>
-                        </div>
-                    </div>
-                </div>
+                <ParentChildSelector isSelected={isParentClicked} onClick={() => {
+                        setParentClicked(true);
+                        setChildClicked(false);
+                    }}
+                    text="부모입니다."
+                    description="자녀에게 용돈을 줄거에요."
+                    />
+                <ParentChildSelector isSelected={isChildClicked} onClick={() => {
+                        setParentClicked(false);
+                        setChildClicked(true);
+                    }}
+                    text="자녀입니다."
+                    description="용돈을 받고 금융도 배울거에요."
+                    />
             </div>
             <div className="fixed bottom-5">
                 <ConfirmButton />
