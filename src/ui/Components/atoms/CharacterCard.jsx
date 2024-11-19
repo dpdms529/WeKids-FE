9d4acdf4fff49db732806a15b7625932fa8475fb
jabin1221 <koreanjb1221@gmail.com>
2024-11-19 14:47:56 +0900

    [feat/#45]: 캐릭터 카드 텍스트 children으로 수정

--- src/ui/Components/atoms/CharacterCard.jsx
+++ src/ui/Components/atoms/CharacterCard.jsx
@@ -2,7 +2,8 @@ const CharacterCard = ({
     width = "w-full",
     height = "h-[513px]",
     radius = "rounded-[10px]",
-    border = "border-black"
+    border = "border-black",
+    children
 }) => {
     return (
         <div className={`flex flex-col bg-main02 gap-12 ${height} ${width} ${border} ${radius} border-2`}>
@@ -11,8 +12,7 @@ const CharacterCard = ({
                 <img src="/images/하츄핑.png"></img>
             </div>
             <div className="flex flex-col items-center gap-4 h-2/5">
-                <span className="text-white text-B-28">가족 관계 확인 중입니다.</span>
-                <span className="text-white text-R-14">2분 정도 걸릴 수 있어요.</span>
+                {children}
             </div>
             
         </div>
