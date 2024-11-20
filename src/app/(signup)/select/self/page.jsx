6ca81e4a9c045606eafc5859d9859c929aef7c7c
jabin1221 <koreanjb1221@gmail.com>
2024-11-20 15:33:11 +0900

    [feat/#37]: 전체 동의, 필수 동의, 선택 동의 구현

--- src/app/(signup)/select/self/page.jsx
+++ src/app/(signup)/select/self/page.jsx
@@ -6,6 +6,7 @@ import CustomButton from "@/src/ui/Components/atoms/CustomButton";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { urlPath } from "@/src/constants/common";
+import toast, { Toaster } from "react-hot-toast";
 
 
 export default function Page() {
@@ -13,19 +14,23 @@ export default function Page() {
     const [bottomChecked, setBottomChecked] = useState(false);
     const router = useRouter();
 
+    const notify = () => {
+        toast('빈칸을 채워주세요!');
+      };
+
     return (
+
+
         <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
+            <Toaster position="top-center" />   
             <Top setAllChecked={setTopChecked}/>
             <Bottom setAllChecked={setBottomChecked}/>
             <div className="px-10 py-5">
                 <CustomButton 
                     rounded="true" 
-                    className={`w-full ${(topChecked == true && bottomChecked == true)  ? "bg-main02" : "bg-stone-300 hover:bg-stone-300 pointer-events-none"}`} 
-                    disabled={!(topChecked == true && bottomChecked == true)}
+                    className={`w-full ${(topChecked == true && bottomChecked == true)  ? "bg-main02" : "bg-stone-300 hover:bg-stone-300"}`} 
                     onClick={() => {
-                        if (topChecked && bottomChecked) {
-                            router.push(urlPath.HOME);
-                        }
+                       topChecked && bottomChecked ? router.push(urlPath.HOME) : notify()
                     }}
                 >
                     다음
