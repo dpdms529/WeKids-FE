2e34e30076e4f43fc54f8f9b263d6bd27cb87003
jabin1221 <koreanjb1221@gmail.com>
2024-11-20 23:13:40 +0900

    [feat/#52]: 합계 계산 변수 작성

--- src/app/(signup)/account/page.jsx
+++ src/app/(signup)/account/page.jsx
@@ -6,10 +6,10 @@ import Header from "@/src/ui/layout/Header";
 import {useState} from "react";
 
 const dummyData = [
-    { id: 1, name: "입출금 통장", account: 111-111-111, balance: 1000000 },
-    { id: 2, name: "적금 통장", account: 222-222-222, balance: 1000000 },
-    { id: 3, name: "예금 통장", account: 333-333-333, balance: 1000000 },
-    { id: 4, name: "모임 통장", account: 444-4444-444, balance: 1000000 },
+    { id: 1, name: "입출금 통장", account: "111-111-111", balance: 1000000 },
+    { id: 2, name: "적금 통장", account: "222-222-222", balance: 1000000 },
+    { id: 3, name: "예금 통장", account: "333-333-333", balance: 1000000 },
+    { id: 4, name: "모임 통장", account: "444-4444-444", balance: 1000000 },
   ];
 
 export default function Page() {
@@ -20,9 +20,12 @@ export default function Page() {
       
       const toggleAccountSelection = (index) => {
         setSelectedAccounts((prev) =>
-          prev.map((isSelected, idx) => (idx === index ? !isSelected : isSelected))
+          prev.map((isSelected, idx) => (idx == index ? !isSelected : isSelected))
         );
       };
+
+      const totalBalance = dummyData.reduce((acc, item) => acc + item.balance, 0);
+      const itemCount = dummyData.length;
 
     return (
         <div className="flex flex-col bg-white h-screen max-w-[393px] overflow-y-hidden">
@@ -34,8 +37,8 @@ export default function Page() {
                 </div>
                 <div className="flex">
                 <CustomButton rounded={true} color={"gray"} className="pointer-events-none h-10 flex items-center justify-between px-4" >
-                    <span className="text-left">총 4 개</span>
-                    <span className="text-right">40000원</span>
+                    <span className="text-left">총 {itemCount} 개</span>
+                    <span className="text-right">{totalBalance}원</span>
                 </CustomButton>
                 </div>
             </div>
@@ -43,9 +46,9 @@ export default function Page() {
                 {dummyData.map((account, index) => (
                     <ParentChildSelector
                     key={account.id}
-                    isSelected={selectedAccounts[index]} // 선택 상태
+                    isSelected={selectedAccounts[index]}
                     className="my-4"
-                    onClick={() => toggleAccountSelection(index)} // 클릭 이벤트
+                    onClick={() => toggleAccountSelection(index)}
                 >
                     <SelectorAccount name={account.name} account={account.account} balance={account.balance} />
                 </ParentChildSelector>
