18f5a05116c86bf4e1a356c95e10c68592174e04
jabin1221 <koreanjb1221@gmail.com>
2024-11-20 20:37:40 +0900

    [feat/#52]: 초기 세팅

---
+++ src/app/(signup)/account/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Header from "@/src/ui/layout/Header";
+
+export default function Page() {
+
+    
+
+    return (
+        <div className="flex flex-col bg-white h-screen max-w-[393px] overflow-y-hidden">
+            <Header />
+            
+        </div>
+    );
+}
