c2b37cb54315b888698d6db8e6585f1b70286706
jabin1221 <koreanjb1221@gmail.com>
2024-11-20 16:18:32 +0900

    [feat/#39]: 체크 원 구현

--- src/app/(signup)/select/self/confirm/page.jsx
+++ src/app/(signup)/select/self/confirm/page.jsx
@@ -5,6 +5,14 @@ export default function Page() {
     return (
         <div className="flex flex-col bg-white overflow-hidden h-screen max-w-[393px]">
             <div className="flex flex-col p-[40px] h-full">
+                <div className="flex flex-col gap-[26px] h-5/6 justify-center items-center">
+                    <div>
+                        <div className="flex rounded-full w-[55px] h-[55px] bg-main02 justify-center items-center">
+                            <CheckIcon className="flex w-[49px] h-[43px] text-white "/>
+                        </div>
+                    </div>
+                    
+                </div>
                 
             </div>
         </div>
