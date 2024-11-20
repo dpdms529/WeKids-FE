c68679416a2d62f923f5a786f63982f4912f39a8
yoon <cbj0010@naver.com>
2024-11-20 16:05:17 +0900

    [file/#28]: lint적용

--- src/constants/transaction.js
+++ src/constants/transaction.js
@@ -1,22 +1,22 @@
 export const RangeEnum = {
-  ONE_MONTH: "1개월",
-  THREE_MONTHS: "3개월",
-  LAST_MONTH: "지난달",
-  CUSTOM: "직접설정",
+    ONE_MONTH: "1개월",
+    THREE_MONTHS: "3개월",
+    LAST_MONTH: "지난달",
+    CUSTOM: "직접설정",
 };
 
 export const TypeEnum = {
-  ALL: "전체",
-  DEPOSIT: "입금만",
-  WITHDRAWAL: "출력만",
+    ALL: "전체",
+    DEPOSIT: "입금만",
+    WITHDRAWAL: "출력만",
 };
 
 export const SortEnum = {
-  NEWEST: "최신순",
-  OLDEST: "과거순",
+    NEWEST: "최신순",
+    OLDEST: "과거순",
 };
 
 export const AccountTransactionTypeEnum = {
-  DEPOSIT: "입금",
-  WITHDRAWAL: "출력",
+    DEPOSIT: "입금",
+    WITHDRAWAL: "출력",
 };
