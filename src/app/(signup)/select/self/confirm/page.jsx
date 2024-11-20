f945c1cd926bf9b62b78f24c6b780e126912be16
jabin1221 <koreanjb1221@gmail.com>
2024-11-20 16:17:01 +0900

    [feat/#39]: 초기 뷰의 틀 구현

---
+++ src/app/(signup)/select/self/confirm/page.jsx
@@ -0,0 +1,12 @@
+import CustomButton from "@/src/ui/Components/atoms/CustomButton";
+import { CheckIcon } from "@radix-ui/react-icons";
+
+export default function Page() {
+    return (
+        <div className="flex flex-col bg-white overflow-hidden h-screen max-w-[393px]">
+            <div className="flex flex-col p-[40px] h-full">
+                
+            </div>
+        </div>
+    );
+}
