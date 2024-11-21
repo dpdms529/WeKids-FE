991950c9f509af21a1cacd716f9d41f8297055cd
jabin1221 <koreanjb1221@gmail.com>
2024-11-21 23:20:08 +0900

    [feat/#56]: 중앙 뷰 세팅

--- src/app/card/design/page.jsx
+++ src/app/card/design/page.jsx
@@ -8,6 +8,13 @@ const Page = () => {
                 <ArrowLeftIcon className="size-8 text-white cursor-pointer"/>
             </div>
             <div className="flex flex-col p-4 h-full">
+                <div className="flex flex-col h-5/6 items-center justify-center">
+                <div className="flex flex-col items-center gap-10">
+                    <div className="text-R-28 text-white">자녀가 선택한 디자인</div>
+                    <div className="w-[196px] h-[312px] bg-red-100"></div>
+                </div>
+                    
+                </div>
                 
             </div>
         </div>
