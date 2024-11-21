c691079ebb64a8ead1fd93d284fe88909643e5db
jabin1221 <koreanjb1221@gmail.com>
2024-11-21 17:24:40 +0900

    [feat/#55]: 중앙 뷰 구현

--- src/app/card/page.jsx
+++ src/app/card/page.jsx
@@ -9,7 +9,18 @@ export default function Page() {
         <div className="flex flex-col h-screen max-w-full bg-white overflow-hidden">
             <Header />
             <div className="flex flex-col h-4/5 justify-end ">
-            
+            <div className="flex flex-row border-2 border-black rounded-xl w-2/3 h-1/2 ml-auto mr-10">
+                
+            </div>
+
+            <div className="flex flex-col pb-28">
+            <Image
+            src="/images/chachapingImg.svg"
+            alt="character image"
+            width={150}
+            height={150}
+            />
+            </div>
             
             </div>
             <div className="flex flex-col h-1/5 justify-center px-10">
