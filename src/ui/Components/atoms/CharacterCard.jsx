4a5fa617da1888587f8a623242af098a721222bb
jabin1221 <koreanjb1221@gmail.com>
2024-11-22 12:20:27 +0900

    [feat/#45]: 클래스 네임 추가

--- src/ui/Components/atoms/CharacterCard.jsx
+++ src/ui/Components/atoms/CharacterCard.jsx
@@ -6,10 +6,11 @@ const CharacterCard = ({
     radius = "rounded-[10px]",
     border = "border-black",
     imagePath="/images/avatarImg.svg",
-    children
+    children,
+    className
 }) => {
     return (
-        <div className={`flex flex-col bg-main02 gap-12 ${height} ${width} ${border} ${radius} border-2`}>
+        <div className={`flex flex-col bg-main02 gap-12 ${height} ${width} ${border} ${radius} border-2 ${className}`}>
             
             <div className="flex flex-col items-center justify-end h-3/5 bottom-0">
                 <Image src={imagePath} alt="profile image" layout="intrinsic" width={100} height={100} />
