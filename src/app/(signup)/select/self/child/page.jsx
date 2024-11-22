8f92bdb5dd4b16f90d9befb51148855abb8a8bb5
jabin1221 <koreanjb1221@gmail.com>
2024-11-22 17:49:16 +0900

    [feat/#63]: 초기 세팅(회원가입 화면 응용)

---
+++ src/app/(signup)/select/self/child/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Top from "@/src/ui/Components/signup/Top"
+import Bottom from "@/src/ui/Components/signup/Bottom"
+import CustomButton from "@/src/ui/Components/atoms/CustomButton";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { urlPath } from "@/src/constants/common";
+import toast, { Toaster } from "react-hot-toast";
+
+
+export default function Page() {
+    const [topChecked, setTopChecked] = useState(false);
+    const [bottomChecked, setBottomChecked] = useState(false);
+    const router = useRouter();
+
+    const notify = () => {
+        toast('빈칸을 채워주세요!');
+      };
+
+    return (
+      <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
+        <Toaster position="top-center" />   
+        <div>
+
+        </div>
+        <Bottom setAllChecked={setBottomChecked}/>
+        <div className="flex flex-col px-10 py-5 gap-2">
+          <CustomButton 
+              rounded="true" 
+              className={`w-full ${(topChecked == true && bottomChecked == true)  ? "bg-main02" : "bg-stone-300 hover:bg-stone-300"}`} 
+              onClick={() => {
+                  topChecked && bottomChecked ? router.push(urlPath.HOME) : notify()
+              }}
+          >
+              다음
+          </CustomButton>
+          <CustomButton 
+              rounded="true" 
+              className={`w-full ${(topChecked == true && bottomChecked == true)  ? "bg-main02" : "bg-stone-300 hover:bg-stone-300"}`} 
+              onClick={() => {
+                  topChecked && bottomChecked ? router.push(urlPath.HOME) : notify()
+              }}
+          >
+              닫기
+          </CustomButton>
+        </div>
+      </div>
+    );
+}
