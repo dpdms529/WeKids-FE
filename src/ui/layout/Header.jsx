e7ffa46c87b2e7dcb0b8e13d2716a8bbed66aeb0
yoon <cbj0010@naver.com>
2024-11-23 01:40:08 +0900

    [feat/#60]: header alarm icon 수정
    알림 아이콘 수정, 알람 개수에 따라서 보여지는 개수 추가

--- src/ui/layout/Header.jsx
+++ src/ui/layout/Header.jsx
@@ -1,12 +1,14 @@
 // components/Header.js
 import { urlPath } from "@/src/constants/common";
-import { EnvelopeClosedIcon, PersonIcon } from "@radix-ui/react-icons";
+import { BellIcon, PersonIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
+  const notificationCount = 0; // 알림 개수를 상태로 관리하거나 props로 받을 수 있습니다
+
   return (
-    <header className="flex justify-between items-center pr-7 pl-6 ">
+    <header className="flex justify-between items-center pr-7 pl-6">
       <Link href={urlPath.HOME}>
         <Image
           width={0}
@@ -16,9 +18,16 @@ export default function Header() {
           className="w-fit h-fit mr-5 mt-5"
         />
       </Link>
-      <div className="flex space-x-4 items-baseline mt-10">
-        {/* 알람 아이콘 */}
-        <EnvelopeClosedIcon className="h-5 w-5 cursor-pointer" />
+      <div className="flex space-x-3 items-baseline mt-10">
+        {/* 알람 아이콘과 배지 */}
+        <div className="relative">
+          <BellIcon className="h-5 w-5 cursor-pointer" />
+          {notificationCount > 0 && (
+            <div className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">
+              {notificationCount}
+            </div>
+          )}
+        </div>
         {/* 프로필 아이콘 */}
         <PersonIcon className="h-5 w-5 mr-4 cursor-pointer" />
       </div>
