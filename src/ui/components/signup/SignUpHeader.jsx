0f042efc089c5a361ff47f2d8b3c560fd14a9465
jabin1221 <koreanjb1221@gmail.com>
2024-11-23 15:09:39 +0900

    [feat/#63]: 수정

--- src/ui/components/signup/SignUpHeader.jsx
+++ src/ui/components/signup/SignUpHeader.jsx
@@ -2,34 +2,44 @@
 
 import React, {useState, useEffect} from "react";
 import CustomButton from "@/src/ui/Components/atoms/CustomButton";
-import InputTextBox from "../atoms/InputTextBox";
+import InputTextBox from "@/src/ui/Components/atoms/InputTextBox";
+import { useRouter } from "next/navigation";
+import { urlPath } from "@/src/constants/common";
 
 export default function Top ({setAllChecked}){
 
-    const [phoneChecked, setphoneChecked] = useState(true);
+    const [phoneChecked, setphoneChecked] = useState(false);
+    const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
+    const router = useRouter();
     
     useEffect(() => {
-        setAllChecked(email !== "" && name !== "" && !phoneChecked);
-    }, [phoneChecked, email, name, setAllChecked]);
+        email != "" && name != "" ? setphoneChecked(true) : setphoneChecked(false) 
+        setAllChecked(email !== "" && name !== "" && phoneChecked);
+        
+    }, [phone, email, name, setAllChecked]);
+
+    const PhoneClickListener = () =>{
+        router.push(urlPath.SIGNUP_PHONE); // 추후에 혹시라도 zustand 쓸수도 있어서 함수로
+    }
 
     return(
-        <>
+        <> 
             <div className="px-5 py-2">
-                <h2>개인정보 입력</h2>
+            <a className="flex text-R-20 text-black/80 mb-10">개인정보 입력</a>
             </div>
             <div className="px-10 py-4">
-                <h2>이메일</h2>
+            <a className="flex text-R-20 text-black/80 mb-3">이메일</a>
                 <InputTextBox value={email} onChange={setEmail}/>
             </div>
             <div className="px-10 py-4">
-                <h2>이름</h2>
+                <a className="flex text-R-20 text-black/80 mb-3">이름</a>
                 <InputTextBox value={name} onChange={setName}/>
             </div>
             <div className="px-10 py-4">
-                <h2>휴대폰 인증</h2>
-                <CustomButton key={phoneChecked} rounded = "true" className={`w-full ${phoneChecked ? "bg-main02" : "bg-stone-300 hover:bg-stone-300 pointer-events-none"}`} onClick={() => setphoneChecked(false)}>
+            <a className="flex text-R-20 text-black/80 mb-3">휴대폰 인증</a>
+                <CustomButton key={phoneChecked} rounded = "true" className={`w-full border border-black/80 ${phoneChecked ? "bg-main01" : "bg-stone-300 hover:bg-stone-300 pointer-events-none"}`} onClick={PhoneClickListener}>
                      휴대폰 인증하기
                 </CustomButton> 
             </div>
