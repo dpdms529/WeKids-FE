59d341a5d6b7dff613d4f40240d4e45bc59473af
jabin1221 <koreanjb1221@gmail.com>
2024-11-24 20:01:38 +0900

    [feat/#65]: 미션 이름 변수 설정

--- src/ui/components/atoms/AlarmCard.jsx
+++ src/ui/components/atoms/AlarmCard.jsx
@@ -11,13 +11,11 @@ const EMOTICON = {
 
 const AlarmCard = ({
     case : number = 1 ,
+    missionName = "설거지 미션",
     width = "w-full",
     height = "h-[513px]",
     radius = "",
     border = "",
-    emoticon="MESSAGE",
-    title = "완료 미션 바로 보기",
-    description = `미션을 확인해보세요. <br/>아이가 XX미션을 완료했습니다.`,
     date = "11월 21일",
     className
 }) => {
@@ -25,15 +23,15 @@ const AlarmCard = ({
   const data = alarmData[number];
   if(!data) return null;
 
-    const SelectedIcon = EMOTICON[emoticon];
+    const SelectedIcon = EMOTICON[data.emoticon];
     return (
       <div className={`flex flex-row ${width} ${height} ${radius} ${border} bg-main03 h-[149px] p-6 gap-5 ${className}`}>
         <div className="flex items-start">
           {SelectedIcon && <SelectedIcon className="w-[23px] h-[23px]" />} 
         </div>
         <div className="flex flex-col gap-3 w-3/4">
-          <div className="text-B-18 text-black">{title}</div>
-          <div className="text-R-14 text-black/60">{description.split("<br/>").map((line, index) => (
+          <div className="text-B-18 text-black">{data.title}</div>
+          <div className="text-R-14 text-black/60">{data.description(missionName).split("<br/>").map((line, index) => (
                 <p key={index}>{line}</p>
             ))}</div>
           <div className="text-R-10 text-black/40">{date}</div>
