d38a4812cc7313b96315f7926a521092edcca0e2
jabin1221 <koreanjb1221@gmail.com>
2024-11-24 18:40:55 +0900

    [feat/#65]: constants로 분리

--- src/ui/components/atoms/AlarmCard.jsx
+++ src/ui/components/atoms/AlarmCard.jsx
@@ -1,4 +1,5 @@
 import { ChevronRightIcon, EnvelopeClosedIcon, CheckIcon, DocumentIcon, CardStackIcon, SpeakerLoudIcon } from '@radix-ui/react-icons';
+import { alarmData } from '@/src/constants/assign';
 
 const EMOTICON = {
     MESSAGE: EnvelopeClosedIcon,
@@ -8,35 +9,8 @@ const EMOTICON = {
     ANNOUNCEMENT: SpeakerLoudIcon,
 };
 
-const alarmData = [
-  {
-      emoticon: "COMPLETED",
-      title: "완료 미션 바로 보기",
-      description: `미션을 확인해보세요. <br/>아이가 XX미션을 완료했습니다.`,
-        },
-  {
-      emoticon: "CARD",
-      title: "등록된 카드 디자인 확인하기",
-      description: `아이가 만든 카드 디자인을 확인하고 카드를 발행해주세요!`,
-  },
-  {
-      emoticon: "DOCUMENT",
-      title: "협상 제안서 바로 보기",
-      description: `용돈 협상 제안서를 확인해보세요. <br/>아이가 용돈 협상 제안서를 등록했습니다!`,
-  },
-  {
-      emoticon: "ANNOUNCEMENT",
-      title: "휴일에도 자동이체가 가능합니다!",
-      description: `2024년 10월 7일부터 휴일에도 자동이체가 가능해요!`,
-  },
-  {
-      emoticon: "MESSAGE",
-      title: "부모 동의 알림 확인하기",
-      description: `가족관계 증명서 확인해주기. <br/>아이가 소셜로그인 가입을 위한 부모 동의 알림을 보냈습니다!`,
-  },
-];
-
 const AlarmCard = ({
+    case : number = 1 ,
     width = "w-full",
     height = "h-[513px]",
     radius = "",
@@ -47,6 +21,10 @@ const AlarmCard = ({
     date = "11월 21일",
     className
 }) => {
+
+  const data = alarmData[number];
+  if(!data) return null;
+
     const SelectedIcon = EMOTICON[emoticon];
     return (
       <div className={`flex flex-row ${width} ${height} ${radius} ${border} bg-main03 h-[149px] p-6 gap-5 ${className}`}>
