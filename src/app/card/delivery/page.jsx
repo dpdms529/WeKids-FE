a0149c4c3f168b93cf151c40e4a3596853d50a22
jabin1221 <koreanjb1221@gmail.com>
2024-11-25 01:45:40 +0900

    [feat/#67]: 모달창 띄우기

--- src/app/card/delivery/page.jsx
+++ src/app/card/delivery/page.jsx
@@ -5,14 +5,20 @@ import InputTextBox from "@/src/ui/components/atoms/InputTextBox";
 import Script from "next/script";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import Modal from "@/src/ui/components/atoms/Modal";
+import ConfirmButton from "@/src/ui/components/transaction/detail/ConfirmButton";
 
 export default function Page() {
 
   const [postcode, setPostcode] = useState("");
   const [address, setAddress] = useState("");
-
+  const [isOpen, setIsOpen] = useState(false);
   const notify = () => {
-    toast("주소가 정상적으로 입력되었습니다!");
+    toast(
+      <div>
+        주소가 입력되지 않았습니다. <br /> 다시 입력해주세요.
+      </div>
+    );
   };
 
 
@@ -23,10 +29,11 @@ export default function Page() {
       oncomplete: function (data) {
         setPostcode(data.zonecode);
         setAddress(data.address); // 우선 이것들만
+        setIsOpen(true);
         
       },
       onclose: function(state) {
-        if(state === 'COMPLETE_CLOSE'){
+        if(state === 'FORCE_CLOSE'){
             notify();
         }
       }
@@ -36,6 +43,10 @@ export default function Page() {
       top: (window.screen.height / 2) - (height / 2)
     });
   };
+
+  const modalHandler = () =>{
+    setIsOpen(!isOpen);
+  }
 
   
 
@@ -49,21 +60,38 @@ export default function Page() {
       <div className="flex flex-col items-center gap-2">
         <div className="flex flex-col h-1/2">
           <div className="flex flex-col items-center">배송지 선택하기</div>
-          <div className="flex w-[196px] h-[312px] bg-red-100"></div>
+          <div className="flex w-[196px] h-[312px] bg-red-100 mt-6"></div>
         </div>
         <div className="flex flex-col h-1/2">
           <div className="flex flex-col h-[130px]">
-            <div>배송지를 입력해 주세요.</div>
+            <div className="mb-3">배송지를 입력해 주세요.</div>
             <CustomButton onClick={searchPostCodeHandler} size={"mediumLarge"} rounded={true}>
                 우편번호
             </CustomButton>
+          <Modal isOpen={isOpen}
+            modalHandler={modalHandler}
+            border="rounded-3xl"
+            bottom="bottom-[332px]" 
+            width="w-[393px]" 
+            height="h-[208px]"
+            deletebutton={true}>
+              <div className="flex flex-col w-full h-full justify-center items-center gap-5 mt-12">
+                <div className="text-R-20 text-black">배송지 등록 완료</div>
+                <div className="text-R-14 text-black/60">배송지 등록이 완료 되었습니다.</div>
+                <CustomButton size="mediumLarge" rounded={true} onClick={modalHandler} >
+                  확인
+                  </CustomButton>
+              </div>
+            
+          </Modal>
+              
           </div>
           <div className="flex flex-col h-[130px]">
-            <div>받는 분 이름을 입력해 주세요.</div>
+            <div className="mb-3">받는 분 이름을 입력해 주세요.</div>
             <InputTextBox />
           </div>
           <div className="flex flex-col h-[130px]">
-            <div>연락처를 입력해 주세요.</div>
+            <div className="mb-3">연락처를 입력해 주세요.</div>
             <InputTextBox />
           </div>
           <div className="flex flex-col items-center h-[102px]"> 
--- src/ui/components/atoms/Modal.jsx
+++ src/ui/components/atoms/Modal.jsx
@@ -2,33 +2,28 @@ export default function Modal({
   isOpen,
   modalHandler,
   children,
-  width = "30%",
-  height = "70vh",
-  translateY = "0%",
+  width = 'w-[393px]', 
+  height='h-[443px]',
+  bottom = 'bottom-0', 
   deletebutton = false,
+  border='rounded-t-3xl',
+  button_color='text-gray-500',
+  button_isLeft = 'true'
 }) {
   if (!isOpen) return null;
   return (
-    <div
-      className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center z-50"
-      onClick={modalHandler}
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className={`bg-white rounded-t-3xl p-6 transform transition-transform duration-500 ease-in-out 
-                visible opacity-100
-                 absolute bottom-0`}
-        style={{ width, height, transform: `translateY(${translateY})` }}
-      >
-        <div className="flex justify-end items-center mb-4">
-          {delete_button && (
-            <button onClick={modalHandler} className="text-gray-500">
-              ✕
-            </button>
-          )}
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center z-50" onClick={modalHandler} >
+            <div onClick={(e) => e.stopPropagation()}
+                className={`bg-white ${border} transform transition-transform duration-500 ease-in-out ${
+                    isOpen ? "visible opacity-100" : "invisible opacity-0"
+                } absolute ${bottom} ${width} ${height}`}
+            >
+                <div className={`fixed ${button_isLeft ? "top-[19px] left-[19px]" : "top-[19px] right-[19px]"}`}>
+                {deletebutton && <button onClick={modalHandler} className = {`${button_color} text-R-14`}>✕</button>}
+                
+                </div>
+                <div onClick={(e) => e.stopPropagation()}>{children}</div>
+            </div>
         </div>
-        <div onClick={(e) => e.stopPropagation()}>{children}</div>
-      </div>
-    </div>
-  );
-}
+    );
+};
