4f64328852d78d5fd670d8f1411daf4dbb541c16
안찬웅 <1stevering@naver.com>
2024-11-24 20:51:07 +0900

    [refact/#54]: 카드 캐릭터 버튼 캐릭터 확대

--- src/ui/Components/card-select/CharacterButton.jsx
+++ src/ui/Components/card-select/CharacterButton.jsx
@@ -3,8 +3,11 @@ import Profile from '@/src/ui/Components/atoms/Profile';
 
 const CharacterButton = ({ character, imagePath, onClick }) => {
   return (
-    <button onClick={onClick} className="w-20 h-20 cursor-pointer"> 
-      <Profile imagePath={imagePath} className="w-full h-full" />
+    <button onClick={onClick} className="w-20 h-20 cursor-pointer overflow-hidden rounded-full"> 
+      <Profile 
+        imagePath={imagePath} 
+        className="w-full h-full object-cover transform scale-150 translate-x-[+00%] translate-y-[+20%]" 
+      />
     </button>
   );
 };
