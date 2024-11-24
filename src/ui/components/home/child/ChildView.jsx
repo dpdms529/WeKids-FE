b79da824fdc2b8646ece3fe2d85744ee785b4b37
yoon <cbj0010@naver.com>
2024-11-25 04:32:57 +0900

    [feat/#60] : 시작지점 나누기
    부모입장일때 아이 입장일때 시작하는 뷰 나누기

---
+++ src/ui/components/home/child/ChildView.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+const childAccountData = {
+  children: [
+    {
+      id: 1,
+      accountNumber: "1234-5678-9101-1121",
+      balance: 300000,
+      name: "김민수",
+      state: "active",
+      designType: "GOGOPING",
+    },
+    {
+      id: 2,
+      accountNumber: "2222-3333-4444-5555",
+      balance: 450,
+      name: "박지수",
+      state: "inactive",
+      designType: "HEARTSPRING",
+    },
+  ],
+};
+
+export default function ChildHome() {
+  return <div />;
+}
