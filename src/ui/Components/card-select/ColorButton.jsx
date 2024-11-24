b2bc00a608d8164a524b6f77944889aaf1fdde08
안찬웅 <1stevering@naver.com>
2024-11-24 19:27:56 +0900

    [refact/#54]: ColorButton 테두리 수정

--- src/ui/Components/card-select/ColorButton.jsx
+++ src/ui/Components/card-select/ColorButton.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const ColorButton = ({ colorClass, onClick }) => {
   return (
-    <button onClick={onClick} className={`w-12 h-12 rounded-full ${colorClass} border border-gray-300`}>
-      
+    <button
+      onClick={onClick}
+      className={`w-12 h-12 rounded-full ${colorClass} border border-black/40`}
+    >
     </button>
   );
 };
