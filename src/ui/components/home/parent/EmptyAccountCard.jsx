b41cd75bda62cc246135ff9526802d8f1cfd5777
yoon <cbj0010@naver.com>
2024-11-25 20:00:09 +0900

    [fix/#60] : 경로수정

--- src/ui/components/home/parent/EmptyAccountCard.jsx
+++ src/ui/components/home/parent/EmptyAccountCard.jsx
@@ -1,9 +1,11 @@
+import { urlPath } from "@/src/constants/common";
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import { Flex } from "@radix-ui/themes";
+import Link from "next/link";
 
 const NoAccountCard = ({
   width = "331px",
-  height = "252px",
+  height = "532px",
   mainText = "등록된 계좌가 없어요.",
   subText = "계좌를 먼저 등록해 주세요.",
   buttonText = "계좌 가져오기",
@@ -15,15 +17,17 @@ const NoAccountCard = ({
       direction="column"
       align="center"
       justify="between"
-      className="bg-main01 rounded-[10px] relative overflow-hidden"
+      className="bg-main02 rounded-[10px] relative overflow-hidden border border-black"
       style={{ width, height }}
     >
       <div className="flex-1 flex flex-col items-start justify-center">
         <p className={`${textColor} text-R-20 mb-3`}>{mainText}</p>
         <p className={`${textColor} text-R-20`}>{subText}</p>
       </div>
-      <div className="border-t-2 border-t-black">
-        <CustomButton onClick={onButtonClick}>{buttonText}</CustomButton>
+      <div className="border border-t-black">
+        <Link href={urlPath.SIGNUP_GET_ACCOUNT}>
+          <CustomButton onClick={onButtonClick}>{buttonText}</CustomButton>
+        </Link>
       </div>
     </Flex>
   );
