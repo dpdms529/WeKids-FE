2759d702ace91bf8dd061f8e53595aab2b15ecd4
jabin1221 <koreanjb1221@gmail.com>
2024-11-25 10:36:32 +0900

    [feat/#56]: 피드백 반영

--- src/app/card/design/page.jsx
+++ src/app/card/design/page.jsx
@@ -1,18 +1,16 @@
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
+import ChoiceDesign from "@/src/ui/components/card/ChoiceDesign";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 const Page = () => {
     return(
-        <div className="flex flex-col h-screen max-w-full overflow-hidden bg-main01">
+        <div className="flex flex-col h-screen max-w-full overflow-hidden bg-sub02">
           <div className="flex p-4 h-20">
             <ArrowLeftIcon className="size-8 text-white cursor-pointer"/>
           </div>
           <div className="flex flex-col p-4 h-full">
             <div className="flex flex-col h-5/6 items-center justify-center">
-              <div className="flex flex-col items-center gap-10">
-                <div className="text-R-28 text-white">자녀가 선택한 디자인</div>
-                <div className="w-[196px] h-[312px] bg-red-100"></div>
-              </div> 
+              <ChoiceDesign />
             </div>
             <div className="flex flex-col w-full h-1/6 items-center gap-6">
               <div>
