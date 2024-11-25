57528c9b4c3bc089811b7e46f7eec856b68bdf10
jabin1221 <koreanjb1221@gmail.com>
2024-11-26 01:28:39 +0900

    [feat/#83]: 초기 세팅

---
+++ src/app/card/parent/page.jsx
@@ -0,0 +1,16 @@
+import ChoiceDesign from "@/src/ui/components/card/ChoiceDesign";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col h-screen max-w-full overflow-hidden bg-sub02">
+        <div className="flex flex-col p-4 h-full">
+      <ChoiceDesign text="카드 발급 완료"/>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Page;
+
+
