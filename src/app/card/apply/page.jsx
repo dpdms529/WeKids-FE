8077aff838528466aa07a1a311ccf08b80163cbc
jabin1221 <koreanjb1221@gmail.com>
2024-11-25 23:01:34 +0900

    [feat/#81]: map으로 분할 및 완성

--- src/app/card/apply/page.jsx
+++ src/app/card/apply/page.jsx
@@ -4,61 +4,79 @@ import { assigndata } from "@/src/constants/assign";
 import { PersonIcon, Pencil1Icon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
-
+const imgSrc = {
+  1 : "chachapingImg",
+  2 : "dadapingImg",
+  3 : "gogopingImg",
+  4 : "hachupingImg",
+  5 : "lalapingImg",
+  6 : "happingImg"
+}
 
 export default function Page() {
 
   return (
     <div className="flex flex-col h-screen max-w-full overflow-auto scrollbar-hide">
-        <Header />
-        <div className="flex flex-col p-9">
-            <div className="flex flex-col w-full items-center justify-center h-[384px] rounded-lg bg-main03">
-                <div className="flex flex-col p-12 gap-5">
-                    <div className="flex text-white text-R-14 justify-center">
-                        위키즈와 함께하는
-                    </div>
-                    <div className="flex justify-center text-B-28">
-                        위키즈 체크카드
-                    </div>
-                    <div className="flex flex-col text-white text-R-14 justify-center">
-                        실적한도 제한 없이 어쩌고 저쩌고 <br /> 위키즈 카드에 대한 설명
-                    </div>
-                    <div className="flex flex-row justify-between">
-                        <Image src="/images/familyImg.svg" alt="family" width={25} height={25} />
-                        <div className="h-8 border-l-2 border-white"></div>
-                        <Image src="/images/busImg.svg" alt="bus" width={25} height={25}  />
-                        <div className="h-8 border-l-2 border-white"></div>
-                        <Pencil1Icon className="w-6 h-6" />
-                    </div>
-                </div>
-                <div className="flex flex-row w-full justify-between">
-                    <Image src="/images/chachapingImg.svg" alt="차차핑" width={54} height={54}/>
-                    <Image src="/images/dadapingImg.svg" alt="다다핑" width={54} height={54}/>
-                    <Image src="/images/gogopingImg.svg" alt="고고핑" width={54} height={54}/>
-                    <Image src="/images/hachupingImg.svg" alt="하츄핑" width={54} height={54}/>
-                    <Image src="/images/lalapingImg.svg" alt="라라핑" width={54} height={54}/>
-                    <Image src="/images/happingImg.svg" alt="하핑" width={54} height={54}/>
-                </div>  
+      <Header />
+      <div className="flex flex-col p-9">
+        <div className="flex flex-col w-full pt-3 h-[384px] rounded-lg bg-main03">
+          <div className="flex flex-col p-12 gap-5">
+            <div className="flex text-white text-R-14 justify-center">
+              위키즈와 함께하는
             </div>
-            <div className="flex my-8 justify-center">
-                <CustomButton size="mediumLarge" rounded={true} className="text-R-20 border border-1 border-black">
-                    체크카드 신청하기
-                </CustomButton>
+            <div className="flex justify-center text-B-28 text-white">
+              위키즈 체크카드
             </div>
-            <div className="flex flex-col gap-5">
-                <div className="flex text-R-20 text-black">
-                    쓸 때마다 캐시백, 주말에는 2배!
-                </div>
-                <div className="flex flex-col text-R-10 text-black/40 gap-2">
-                    {assigndata.map((item, idx) => (
-                        <div key={idx}>{item}</div>
-                    ))}
-                </div>
-                
+            <div className="flex text-white text-R-14 justify-center">
+              실적한도 제한 없이 어쩌고 저쩌고 <br /> 위키즈 카드에 대한 설명
             </div>
-            
-
+              <div className="flex flex-row justify-between px-9">
+                <Image 
+                src="/images/familyImg.svg" 
+                alt="family" 
+                width={25} 
+                height={25} />
+                <div className="h-8 border-l-2 border-white"></div>
+                <Image 
+                src="/images/busImg.svg" 
+                alt="bus" 
+                width={25} 
+                height={25}  />
+                <div className="h-8 border-l-2 border-white"></div>
+                <Pencil1Icon 
+                className="w-6 h-6" />
+              </div>
+            </div>
+            <div className="flex flex-row w-full justify-between">
+            {Object.values(imgSrc).map((img, idx) => (
+                <Image 
+                key={idx} 
+                src={`/images/${img}.svg`} 
+                alt={`${img}`} 
+                width={54} 
+                height={54}/>
+            ))}
+            </div>  
+          </div>
+          <div className="flex my-8 justify-center">
+            <CustomButton 
+            size="mediumLarge" 
+            rounded={true} 
+            className="text-R-20 border border-1 border-black">
+              체크카드 신청하기
+            </CustomButton>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div className="flex text-B-20 text-black">
+              쓸 때마다 캐시백,<br /> 주말에는 2배!
+            </div>
+          <div className="flex flex-col text-R-10 text-black/40 gap-2">
+            {assigndata.map((item, idx) => (
+              <div key={idx}>{item}</div>
+              ))}
+          </div>    
         </div>
+      </div>
     </div>
   );
 }
