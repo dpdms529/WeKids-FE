6c874fdc6d90bf688a0a570dd10cd2b62a913b09
jabin1221 <koreanjb1221@gmail.com>
2024-11-25 22:22:02 +0900

    [feat/#81]: 하단 뷰 구현

--- src/app/card/apply/page.jsx
+++ src/app/card/apply/page.jsx
@@ -1,5 +1,7 @@
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import Header from "@/src/ui/layout/Header";
+import { assigndata } from "@/src/constants/assign";
+
 
 export default function Page() {
 
@@ -18,14 +20,23 @@ export default function Page() {
                     <div className="flex text-white">
                         실적한도 제한 없이 어쩌고 저쩌고 <br /> 위키즈 카드에 대한 설명
                     </div>
-                </div>
-                
-                
+                </div>  
             </div>
-            <div className="flex mt-8 justify-center">
+            <div className="flex my-8 justify-center">
                 <CustomButton size="mediumLarge" rounded={true} className="text-R-20">
                     체크카드 신청하기
                 </CustomButton>
+            </div>
+            <div className="flex flex-col gap-5">
+                <div className="flex text-R-20 text-black">
+                    쓸 때마다 캐시백, 주말에는 2배!
+                </div>
+                <div className="flex flex-col text-R-10 text-black/40 gap-2">
+                    {assigndata.map((item, idx) => (
+                        <div key={idx}>{item}</div>
+                    ))}
+                </div>
+                
             </div>
             
 
