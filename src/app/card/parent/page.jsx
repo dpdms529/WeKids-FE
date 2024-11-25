5a44fff808e0fba3dcaed2a518b0f55609997f11
jabin1221 <koreanjb1221@gmail.com>
2024-11-26 01:33:16 +0900

    [feat/#82]: 전체적인 뷰 완성

--- src/app/card/parent/page.jsx
+++ src/app/card/parent/page.jsx
@@ -1,12 +1,15 @@
+import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import ChoiceDesign from "@/src/ui/components/card/ChoiceDesign";
 
 const Page = () => {
   return (
     <div className="flex flex-col h-screen max-w-full overflow-hidden bg-sub02">
-        <div className="flex flex-col p-4 h-full">
-      <ChoiceDesign text="카드 발급 완료"/>
+        <div className="flex flex-col p-4 h-full justify-center items-center gap-12">
+        <ChoiceDesign text="카드 발급 완료!"/>
+        <CustomButton size="mediumLarge" rounded={true} className="text-R-20">
+            카드 비밀번호 등록하기
+        </CustomButton>
       </div>
-      
     </div>
   );
 };
