aa667135a8ac621bd34e2131c79d3a144b0bd7cd
안찬웅 <1stevering@naver.com>
2024-11-26 10:52:51 +0900

    [refact/#79]: [발급신청뷰] tailwind 수정

--- src/app/card/delivery/completed/page.jsx
+++ src/app/card/delivery/completed/page.jsx
@@ -1,4 +1,6 @@
 'use client';
+import { useRouter } from 'next/navigation';
+import { urlPath } from "@/src/constants/common";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import React from 'react';
 import CardCharacter from '@/src/ui/Components/card-select/CardCharacter';
@@ -6,14 +8,15 @@ import CustomButton from '@/src/ui/Components/atoms/CustomButton';
 import Header from '@/src/ui/layout/Header';
 
 const CardIssueComplete = () => {
+    const router = useRouter();
+
     return (
       <div className="flex flex-col justify-between h-screen bg-white">
         <Header />
         <div className="flex-grow flex items-center justify-center">
-            {/* 배경색 bg-[#57A9FB]에서 bg-MainColor로 추후 변경*/}
-            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-[#57A9FB] flex flex-col items-center justify-center">
+            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-main02 flex flex-col items-center justify-center">
             <CardCharacter selectedCharacter="HEARTSPRING" selectedColor="bg-pinkHachu" />      
-                <button className="flex items-center mt-4 w-[79px] h-[26px] flex-shrink-0 rounded-[10px] bg-[#D9D9D9] text-R-10 text-black pl-1">
+                <button className="flex items-center mt-4 w-[79px] h-[26px] flex-shrink-0 rounded-[10px] bg-stone-300 text-R-10 text-black pl-1">
                     <ReloadIcon className="mr-2 transform rotate-180 scale-x-[-1]" />
                     뒷면보기
                 </button>
@@ -25,9 +28,10 @@ const CardIssueComplete = () => {
         
         <div>
           <CustomButton 
-            size="large" 
-            className="w-full"
-            onClick={() => alert('확인 버튼 클릭')}>
+            size={"large"}
+            rounded={false}
+            onClick={() => router.push(urlPath.HOME)}
+          >
             확인
           </CustomButton>
         </div>
