322165c7695fff4ae4ad8402d1960d8db6a4e0a5
안찬웅 <1stevering@naver.com>
2024-11-26 11:27:26 +0900

    [refact/#79]: [발급신청 완료 뷰] 컴포넌트 분리

--- src/app/card/delivery/completed/page.jsx
+++ src/app/card/delivery/completed/page.jsx
@@ -1,9 +1,8 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { urlPath } from "@/src/constants/common";
-import { ReloadIcon } from "@radix-ui/react-icons";
 import React from 'react';
-import CardCharacter from '@/src/ui/Components/card-select/CardCharacter';
+import CardDisplay from '@/src/ui/components/card/CardDisplay';
 import CustomButton from '@/src/ui/Components/atoms/CustomButton';
 import Header from '@/src/ui/layout/Header';
 
@@ -14,16 +13,12 @@ const CardIssueComplete = () => {
       <div className="flex flex-col justify-between h-screen bg-white">
         <Header />
         <div className="flex-grow flex items-center justify-center">
-            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-main02 flex flex-col items-center justify-center">
-            <CardCharacter selectedCharacter="HEARTSPRING" selectedColor="bg-pinkHachu" />      
-                <button className="flex items-center mt-4 w-[79px] h-[26px] flex-shrink-0 rounded-[10px] bg-stone-300 text-R-10 text-black pl-1">
-                    <ReloadIcon className="mr-2 transform rotate-180 scale-x-[-1]" />
-                    뒷면보기
-                </button>
-                <p className="mt-4 text-white text-R-20">
-                    카드 발급을 완료했습니다!
-                </p>
-            </div>
+            <CardDisplay 
+                selectedCharacter="HEARTSPRING" 
+                selectedColor="bg-pinkHachu" 
+                buttonText="뒷면보기" 
+                message="카드 발급을 완료했습니다!" 
+            />
         </div>
         
         <div>
