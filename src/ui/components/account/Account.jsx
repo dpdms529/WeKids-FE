28294cc5d70987f5279c6b6c2d7234b4a8c615b2
jabin1221 <koreanjb1221@gmail.com>
2024-11-26 19:26:48 +0900

    refact: Account 수정

--- src/ui/components/account/Account.jsx
+++ src/ui/components/account/Account.jsx
@@ -1,20 +1,17 @@
-"use client";
-
-import CustomButton from "@/src/ui/components/atoms/CustomButton";
-import Header from "@/src/ui/layout/Header";
 import { useRouter } from "next/navigation";
 import { urlPath } from "@/src/constants/common";
 import AccountItem from "@/src/ui/components/signup/AccountItem";
 import { useState } from "react";
+import CustomButton from "@/src/ui/components/atoms/CustomButton";
 
 export default function Page() {
-  const [selectedIndex, setSelectedIndex] = useState(null);
+
+    const [selectedIndex, setSelectedIndex] = useState(null);
   const router = useRouter();
 
-  return (
-    <div className="flex flex-col bg-white h-screen max-w-[393px] overflow-y-hidden">
-      <Header />
-      <AccountItem
+    return (
+        <>
+        <AccountItem
         selectedIndex={selectedIndex}
         setSelectedIndex={setSelectedIndex}
       />
@@ -27,6 +24,6 @@ export default function Page() {
           가져오기
         </CustomButton>
       </div>
-    </div>
-  );
+      </>
+    );
 }
--- src/ui/components/account/Identification.jsx
+++ src/ui/components/account/Identification.jsx
@@ -1,24 +1,22 @@
-"use client";
-import Header from "@/src/ui/layout/Header";
 import SignUpFooter from "@/src/ui/components/signup/SignUpFooter";
 import { useState } from "react";
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import IdentificationBox from "@/src/ui/components/signup/IdentificationBox";
 import toast, { Toaster } from "react-hot-toast";
 
-export default function Page() {
-  const [assignCheck, setAssignChecked] = useState(false);
+
+export default function Identification({setIsChecked}) {
+
+    const [assignCheck, setAssignChecked] = useState(false);
   const [identificationCheck, setIdentificationCheck] = useState(false);
 
   const notify = () => {
     toast("빈칸을 채워주세요!");
   };
-
-  return (
-    <div className="flex flex-col bg-white h-screen overflow-y-auto scrollbar-hide w-full">
-      <Toaster position="top-center" />
-      <Header />
-      <div className="flex flex-col w-full h-[423px] items-center px-12 pt-12">
+    return (
+        <>
+        <Toaster position="top-center" />
+        <div className="flex flex-col w-full h-[423px] items-center px-12 pt-12">
         <div className="text-R-20">
           계좌를 불러오기 위해서는 <br />
           주민등록 번호가 필요해요!
@@ -31,11 +29,14 @@ export default function Page() {
         <div className="flex flex-col my-12">
           <SignUpFooter setAllChecked={setAssignChecked} />
         </div>
-        <div className="flex h-[91px]">
+        <div className="fixed bottom-0">
           <CustomButton
             onClick={() => {
               if (!(assignCheck && identificationCheck)) {
                 notify();
+              }
+              else{
+                setIsChecked(true);
               }
             }}
             color={assignCheck && identificationCheck ? "main" : "gray"}
@@ -44,6 +45,6 @@ export default function Page() {
           </CustomButton>
         </div>
       </div>
-    </div>
-  );
+      </>
+    );
 }
