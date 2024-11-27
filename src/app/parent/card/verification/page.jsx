622da8185c8c7ece8fe7333461688780c32f4ab7
jabin1221 <koreanjb1221@gmail.com>
2024-11-27 09:18:58 +0900

    fix: 약관 수정 들어간 뷰 일부 수정

--- src/app/parent/card/verification/page.jsx
+++ src/app/parent/card/verification/page.jsx
@@ -4,6 +4,7 @@ import { urlPath } from "@/src/constants/common";
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import ChildInputForm from "@/src/ui/components/signup/ChildInputForm";
 import Bottom from "@/src/ui/components/signup/SignUpFooter";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
@@ -17,33 +18,40 @@ export default function Page() {
     toast("빈칸을 채워주세요!");
   };
 
+  const clickHandler = (e) => {
+    if(!topChecked || !bottomChecked){
+      e.preventDefault();
+    }
+  } 
+
   return (
     <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
       <Toaster position="top-center" />
-      <ChildInputForm setAllChecked={setTopChecked} />
-      <div className="px-10 mt-12 mb-1 text-R-20 text-black/80">
-        아이의 계좌를 만들기 위해 <br /> OOO님의 동의가 필요해요
+      <div className="flex flex-col flex-1 h-5/6 overflow-y-auto scrollbar-hide">
+        <ChildInputForm setAllChecked={setTopChecked} />
+        <div className="mt-12 mb-1 text-R-20 text-black/80">
+          <div className="flex flex-col p-8">아이의 계좌를 만들기 위해 <br /> OOO님의 동의가 필요해요</div>
+          <Bottom setAllChecked={setBottomChecked} />
+        </div>
       </div>
-      <Bottom setAllChecked={setBottomChecked} />
-      <div className="flex flex-col px-10 py-5 gap-6">
+      <div className="flex flex-col px-10 py-5 gap-6 h-1/6">
+        <Link href={urlPath.CARD_VERIFICATION_CONFIRM}>
         <CustomButton
           rounded="true"
           className={`w-full ${topChecked == true && bottomChecked == true ? "bg-main02" : "bg-stone-300 hover:bg-stone-300"}`}
-          onClick={() => {
-            topChecked && bottomChecked ? router.push(urlPath.HOME) : notify();
-          }}
+          onClick={clickHandler}
         >
           다음
         </CustomButton>
+        </Link>
+        <Link href={urlPath.CARD_VERIFICATION_CONFIRM}>
         <CustomButton
           rounded="true"
           className="w-full bg-stone-300 hover:bg-stone-300"
-          onClick={() => {
-            router.push(urlPath.CARD_VERIFICATION_CONFIRM);
-          }}
         >
           닫기
         </CustomButton>
+        </Link>
       </div>
     </div>
   );
