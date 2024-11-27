0f7ac0efa57d5b0331636061164f8b4b1f774bb6
jabin1221 <koreanjb1221@gmail.com>
2024-11-27 09:26:49 +0900

    fix: 수정2

--- src/app/parent/card/verification/page.jsx
+++ src/app/parent/card/verification/page.jsx
@@ -19,38 +19,43 @@ export default function Page() {
   };
 
   const clickHandler = (e) => {
-    if(!topChecked || !bottomChecked){
+    if (!topChecked || !bottomChecked) {
       e.preventDefault();
+      notify();
     }
-  } 
+  };
 
   return (
-    <div className="flex flex-col w-[393px] h-screen overflow-y-auto scrollbar-hide bg-white">
+    <div className="flex flex-col w-[393px] h-screen bg-white overflow-hidden">
       <Toaster position="top-center" />
-      <div className="flex flex-col flex-1 h-5/6 overflow-y-auto scrollbar-hide">
+      <div className="flex flex-1 flex-col overflow-y-auto scrollbar-hide">
         <ChildInputForm setAllChecked={setTopChecked} />
-        <div className="mt-12 mb-1 text-R-20 text-black/80">
-          <div className="flex flex-col p-8">아이의 계좌를 만들기 위해 <br /> OOO님의 동의가 필요해요</div>
-          <Bottom setAllChecked={setBottomChecked} />
+        <div className="mt-12 mb-1 text-R-20 text-black/80 px-8">
+          아이의 계좌를 만들기 위해 <br /> OOO님의 동의가 필요해요
         </div>
+        <Bottom setAllChecked={setBottomChecked} />
       </div>
-      <div className="flex flex-col px-10 py-5 gap-6 h-1/6">
+      <div className="flex flex-col px-10 py-5 gap-6">
         <Link href={urlPath.CARD_VERIFICATION_CONFIRM}>
-        <CustomButton
-          rounded="true"
-          className={`w-full ${topChecked == true && bottomChecked == true ? "bg-main02" : "bg-stone-300 hover:bg-stone-300"}`}
-          onClick={clickHandler}
-        >
-          다음
-        </CustomButton>
+          <CustomButton
+            rounded="true"
+            className={`w-full ${
+              topChecked && bottomChecked
+                ? "bg-main02"
+                : "bg-stone-300 hover:bg-stone-300"
+            }`}
+            onClick={clickHandler}
+          >
+            다음
+          </CustomButton>
         </Link>
         <Link href={urlPath.CARD_VERIFICATION_CONFIRM}>
-        <CustomButton
-          rounded="true"
-          className="w-full bg-stone-300 hover:bg-stone-300"
-        >
-          닫기
-        </CustomButton>
+          <CustomButton
+            rounded="true"
+            className="w-full bg-stone-300 hover:bg-stone-300"
+          >
+            닫기
+          </CustomButton>
         </Link>
       </div>
     </div>
