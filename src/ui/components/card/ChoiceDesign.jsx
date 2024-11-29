33da57c274925a221ed64b394c7b503f1c4221cc
안찬웅 <1stevering@naver.com>
2024-11-29 12:27:07 +0900

    [refact/#79]: 디자인 조회 API

--- src/ui/components/card/ChoiceDesign.jsx
+++ src/ui/components/card/ChoiceDesign.jsx
@@ -3,20 +3,42 @@
 import CustomButton from "@/src/ui/components/atoms/CustomButton";
 import CardCharacter from "@/src/ui/components/card-select/CardCharacter";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { designFetch } from "@/src/services/design";
 
 export default function ChoiceDesign({
   title,
   subText,
   buttonText,
   linkUrl,
-  character = "HAPPYING",
+  character = "HEARTSPRING",
   color = "PINK2",
 }) {
+  const [designData, setDesignData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDesign = async () => {
+      try {
+        setIsLoading(true);
+        const data = await designFetch();
+        setDesignData(data);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchDesign();
+  }, []);
+
   return (
     <div className="flex flex-col items-center gap-10">
       <div className="text-R-28 text-white">{title}</div>
       <div className="w-[196px] h-[312px]">
-        <CardCharacter selectedCharacter={character} selectedColor={color} />
+        <CardCharacter
+          selectedCharacter={designData?.character || character}
+          selectedColor={designData?.color || color}
+        />
       </div>
       <div className="text-white">{subText}</div>
       <Link href={linkUrl}>
