9a61bb8bc70f8b4fcf93b5217145166b62825ac9
안찬웅 <1stevering@naver.com>
2024-11-30 21:05:48 +0900

    [feat/#113]: 상태아이콘 추가 및 컴포넌트 분리

---
+++ src/ui/components/mission/StateIcon.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+const StateIcon = ({ state }) => {
+  const getStateIcon = (state) => {
+    const icons = {
+      ACC: "/images/clean.svg",
+      NEW: "/images/new.svg",
+      PRO: "/images/pencil.svg",
+      REJ: "/images/drawing_pin.svg",
+      DONE: "/images/clean.svg"
+    };
+    return icons[state] || icons.NEW;
+  };
+
+  return (
+    <Image
+      src={getStateIcon(state)}
+      alt={`${state} icon`}
+      width={16}
+      height={16}
+    />
+  );
+};
+
+export default StateIcon;
