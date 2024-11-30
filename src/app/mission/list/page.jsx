94d391f1c03bfc568802cecac0b03f8838dce598
안찬웅 <1stevering@naver.com>
2024-11-30 21:35:41 +0900

    [feat/#113]: 미션등록 버튼, 스크롤바 추가

--- src/app/mission/list/page.jsx
+++ src/app/mission/list/page.jsx
@@ -1,15 +1,29 @@
 "use client";
 import Header from "@/src/ui/layout/Header";
 import MissionList from "@/src/ui/components/mission/MissionList";
+import CustomButton from "@/src/ui/components/atoms/CustomButton";
 
 export default function Page() {
     return (
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen overflow-hidden">
         <Header />
-        <div className="p-4">
+        <div className="p-4 flex-1 overflow-hidden flex flex-col">
           <h2 className="text-lg mb-4">아이 선택 및 필터링 여하고 부분</h2>
-          <MissionList />
+          <div className="flex-1 overflow-y-auto custom-scrollbar">
+            <MissionList />
+          </div>
         </div>
+        
+        <div className="flex justify-center mt-auto mb-4">
+          <CustomButton 
+            size="medium"
+            color="main"
+            rounded={true}
+          >
+            미션등록
+          </CustomButton>
+        </div>
+        
       </div>
     );
   }
