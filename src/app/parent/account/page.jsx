fad2b6c0bb88afadd014e49f5b81883a4a281683
YE-EUN <musiclove529@naver.com>
2024-12-01 07:43:42 +0900

    [refact/#41]: 계좌 조회 동의 뷰 수정 및 API 연결

--- src/app/parent/account/page.jsx
+++ src/app/parent/account/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Identification from "@/src/ui/components/account/Identification";
-import Account from "@/src/ui/components/account/Account";
+import AccountList from "@/src/ui/Components/account/AccountList";
 import Header from "@/src/ui/layout/Header";
 import { useState } from "react";
 
@@ -9,15 +9,9 @@ export default function Page() {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <div className="flex flex-col bg-white w-[393px] h-screen overflow-y-hidden">
+    <div className="flex flex-col w-full h-full">
       <Header />
-      <div className="overflow-y-hidden scrollbar-hide">
-        {isClicked ? (
-          <Account />
-        ) : (
-          <Identification setIsChecked={setIsClicked} />
-        )}
-      </div>
+      {isClicked ? <AccountList /> : <Identification setIsChecked={setIsClicked} />}
     </div>
   );
 }
--- src/ui/components/signup/IdentificationBox.jsx
+++ src/ui/components/signup/IdentificationBox.jsx
@@ -3,21 +3,19 @@
 import IdentificationForm from "./IdentificationForm";
 import { useEffect, useState } from "react";
 
-export default function IdentificationBox({ setChecked, setErrorCode }) {
-  const [identification, setIdentification] = useState("".padStart(13, " "));
-  const [checkidentification, setCheckIdentification] = useState(
-    "".padStart(13, " "),
-  );
+export default function IdentificationBox({
+  setChecked,
+  setErrorCode,
+  identification,
+  setIdentification,
+}) {
+  // const [identification, setIdentification] = useState("".padStart(13, " "));
+  const [checkidentification, setCheckIdentification] = useState("".padStart(13, " "));
 
   useEffect(() => {
-    const isEmpty =
-      identification.includes(" ") || checkidentification.includes(" ");
+    const isEmpty = identification.includes(" ") || checkidentification.includes(" ");
     const isMismatch = identification !== checkidentification;
-    setErrorCode([
-      !isMismatch,
-      !identification.includes(" "),
-      !checkidentification.includes(" "),
-    ]);
+    setErrorCode([!isMismatch, !identification.includes(" "), !checkidentification.includes(" ")]);
     setChecked(!isEmpty && !isMismatch);
   }, [identification, checkidentification, setChecked, setErrorCode]);
 
