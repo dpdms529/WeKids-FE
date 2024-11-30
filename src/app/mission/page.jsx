c358f3733b1aa9fece64e8257b5c5c256e8c9f78
jabin1221 <koreanjb1221@gmail.com>
2024-11-30 10:02:22 +0900

    [feat/#107]: 뷰 스타일 적용

--- src/app/mission/page.jsx
+++ src/app/mission/page.jsx
@@ -4,11 +4,11 @@ import InputTextBox from "@/src/ui/components/atoms/InputTextBox";
 
 export default function Page() {
     return (
-        <div className="flex flex-col w-full h-screen overflow-auto justify-center items-center">
-            <div>
+        <div className="flex flex-col w-full h-screen overflow-y-hidden justify-center items-center p-10">
+            <div className="w-full flex flex-col">
                 미션 등록하기
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col overflow-auto">
                 <div className="flex flex-col">
                     <div>
                         미션명
