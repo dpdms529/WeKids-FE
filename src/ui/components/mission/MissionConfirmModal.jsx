0871125d473e6adae6f38a98fdbdf9efc9bc46bd
jabin1221 <koreanjb1221@gmail.com>
2024-12-01 21:03:44 +0900

    [feat/#107]: 컨텐츠 사이즈 조절

--- src/ui/components/mission/MissionConfirmModal.jsx
+++ src/ui/components/mission/MissionConfirmModal.jsx
@@ -14,14 +14,14 @@ export default function MissionConfirmModal({setParentOpen, setOpen}) {
   return (
     
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white w-[320px] rounded-lg shadow-lg px-6 py-10 relative">
+          <div className="bg-white w-[360px] h-[210px] rounded-lg shadow-lg p-8 pt-12 relative">
             <button
               onClick={() => setOpen(false)}
               className="absolute top-2 right-2 text-gray-600 hover:text-black"
             >
               ✕
             </button>
-            <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col items-center gap-12">
               <p className="text-center text-black text-R-14">
                 작성 중인 미션이 있습니다. <br />
                 그래도 나가시겠습니까?
