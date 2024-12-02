39cd1798ff1961d28035be622f8ae5d48943cb67
안찬웅 <1stevering@naver.com>
2024-12-02 15:48:58 +0900

    [refact/#108]: use client 삭제

--- src/app/mission/transfer/done/page.jsx
+++ src/app/mission/transfer/done/page.jsx
@@ -1,29 +1,17 @@
-"use client";
 import { urlPath } from "@/src/constants/common";
 import TransferComplete from "@/src/ui/components/transfer/TransferComplete";
-import { useState } from "react";
 import Link from "next/link";
 
-const Page = () => {
-  const [transferData, setTransferData] = useState({
+export default async function Page() {
+  
+  // const data = await getXXXXX(); 데이터 가져오기
+  const transferData = {
     sendUser: "최윤정",
     amount: 10000,
     accountNumber: "1234567890123",
     bankName: "우리",
     memo: "메모입력..",
-  });
-
-//   useEffect(() => {
-//     if (selectedMission) {
-//       setTransferData((prev) => ({
-//         ...prev,
-//         sendUser: selectedMission.userName,
-//         accountNumber: selectedMission.accountNumber,
-//         amount: 10000, // 미션 금액이 10,000원으로 고정
-//         memo: "미션 수행 완료",
-//       }));
-//     }
-//   }, [selectedMission]);
+  };
 
   return ( // 미션 목록으로 이동하게 변경
     <Link href={urlPath.HOME}> 
@@ -33,5 +21,3 @@ const Page = () => {
     </Link>
   );
 };
-
-export default Page;
