5df0a0a632b95d359b67e737b7794195b0f6199a
jabin1221 <koreanjb1221@gmail.com>
2024-12-02 17:32:41 +0900

    [fix/#107]: Conflict 해결

--- src/app/(transaction)/transaction-history/[trxId]/page.jsx
+++ src/app/(transaction)/transaction-history/[trxId]/page.jsx
@@ -1,9 +1,6 @@
-"use client";
+'use client';
 import { use } from "react";
-import {
-  useTransactionDetail,
-  useUpdateTransactionMemo,
-} from "@/src/services/transaction";
+import { useTransactionDetail, useUpdateTransactionMemo } from "@/src/services/transaction";
 import Loader from "@/src/ui/components/atoms/Loader";
 import ShareButton from "@/src/ui/components/atoms/Sharebutton";
 import Memo from "@/src/ui/components/transaction/detail/Memo";
@@ -21,7 +18,7 @@ const TransactionDetailPage = ({ params }) => {
   const router = useRouter();
 
   useEffect(() => {
-    if (data?.memo && memo === "") {
+    if (data?.memo) {
       setMemo(data.memo);
     }
   }, [data]);
@@ -30,22 +27,27 @@ const TransactionDetailPage = ({ params }) => {
 
   const handleUpdateMemo = () => {
     console.log("HandleUpdateMemo called");
-    if (!trxId || !memo.trim()) {
+    if (!trxId) {
       return;
     }
-
-    mutate(
-      { transactionId: trxId, memo },
-      {
-        onSuccess: () => {
-          console.log("메모 업데이트 성공!");
-          router.push(`${urlPath.TRANSACTION_HISTORY}?color=YELLOW`);
-        },
-        onError: (error) => {
-          console.error("메모 업데이트 실패:", error.message);
-        },
-      },
-    );
+    if(memo == ""){
+      router.push(`${urlPath.TRANSACTION_HISTORY}?color=YELLOW`);
+    }
+    else{
+      mutate(
+        { transactionId: trxId, memo },
+        {
+          onSuccess: () => {
+            console.log("메모 업데이트 성공!");
+            router.push(`${urlPath.TRANSACTION_HISTORY}?color=YELLOW`);
+          },
+          onError: (error) => {
+            console.error("메모 업데이트 실패:", error.message);
+          },
+        }
+      );
+    }
+    
   };
 
   if (isLoading) {
@@ -59,15 +61,14 @@ const TransactionDetailPage = ({ params }) => {
   return (
     <div className="bg-white w-full h-full flex flex-col items-center justify-between">
       <div className="w-full px-5 py-10">
-        <div className="w-full flex items-center text-B-22 my-5">{data.title}</div>
+        <div className="w-full flex items-center text-B-22 my-5">
+          {data.title}
+        </div>
         <Memo memo={memo} setMemo={setMemo} />
         <hr />
         <div className="w-full">
           <TransactionDetail label="거래시각" value={data.createdAt} />
-          <TransactionDetail
-            label="거래구분"
-            value={data.type === "DEPOSIT" ? "입금" : "출금"}
-          />
+          <TransactionDetail label="거래구분" value={data.type === "DEPOSIT" ? "입금" : "출금"} />
           <TransactionDetail label="거래금액" value={data.amount} />
           <TransactionDetail label="거래 후 잔액" value={data.balance} />
         </div>
@@ -77,7 +78,7 @@ const TransactionDetailPage = ({ params }) => {
         <ShareButton />
         <CustomButton onClick={handleUpdateMemo} isLoading={isUpdating}>
           확인
-        </CustomButton>
+          </CustomButton>
       </div>
     </div>
   );
