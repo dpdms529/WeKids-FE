36964bdcec94ca78e4de02cc066dfcff52fd0770
yoon <cbj0010@naver.com>
2024-12-03 00:19:27 +0900

    [feat/#122] : 더미 추가 및 변경
    카드 발급 전

--- src/ui/components/home/child/ChildView.jsx
+++ src/ui/components/home/child/ChildView.jsx
@@ -1,8 +1,5 @@
-"use client";
-
-import { urlPath } from "@/src/constants/common";
-import { PlusIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
+import BlueCardBox from "../BlueCardBox";
+import EmptyAccountCard from "../parent/EmptyAccountCard";
 
 const childAccountData = {
   childId: 1,
@@ -18,7 +15,7 @@ const childAccountData = {
 const childAccountNullData = {
   childId: 1,
   name: "최윤정",
-  accountNumber: "",
+  accountNumber: null,
   profile: "/images/chachapingImg.svg",
   balance: 300000,
   accountId: 2,
@@ -27,18 +24,17 @@ const childAccountNullData = {
   character: "CHACHAPING",
 };
 
-export default function ChildHome() {
-  return (
-    <div className="bg-[#E8F1FF] rounded-[10px] w-[331px] h-[252px] p-6 flex flex-col">
-      <p className="text-B-28 font-medium mb-4">{childAccountData.name}</p>
-      <div className="flex-1">
-        {/* url 수정필요 */}
-        <Link href={urlPath.CHILD_CARD}>
-          <div className="border-2 border-dashed border-black/40 rounded-lg h-full w-full flex items-center justify-center">
-            <PlusIcon width={50} height={50} color="grey" variant="soft" />
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+export default async function ChildHome() {
+  // const data = await getchildAccounts();
+  const data = await childAccountNullData;
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (data.accountNumber === null) {
+    return <EmptyAccountCard name={data.name} />;
+  }
+
+  return <BlueCardBox accountData={data} />;
 }
