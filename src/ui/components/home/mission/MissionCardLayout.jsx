f889c717140cd7ca32513601b7a288c63ecb177e
yoon <cbj0010@naver.com>
2024-12-02 16:04:43 +0900

    [feat/#122] : 부모미션
    부모입장에서의 미션 뷰 구현

---
+++ src/ui/components/home/mission/MissionCardLayout.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+export default function MissionCardLayout({
+  title,
+  subtitle,
+  description,
+  imagePath,
+}) {
+  const [firstPart, secondPart] = description.split("\n");
+
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div className="w-[330px] bg-purple01/30 rounded-xl px-6 pt-5 pb-5">
+        <p className="text-sub02 text-R-14 text-center mb-2">{title}</p>
+        <div className="bg-white rounded-[10px] p-5">
+          {/* <div className="text-L-10 text-right"> */}
+          <p className="text-sub02 text-L-10 text-right block mb-4">
+            {subtitle}
+          </p>
+          <div className="flex">
+            <div className="flex-1 mt-4">
+              <Image
+                src={imagePath}
+                alt="Mission"
+                width={120}
+                height={120}
+                className="object-contain"
+              />
+            </div>
+            <div className="flex-1">
+              <p className="text-B-18 text-right">
+                <span className="text-sub02">{firstPart}</span>
+                <br />
+                <span className="text-main02">{secondPart}</span>
+              </p>
+              <div className="flex justify-end pr-2">
+                <Image
+                  src="/icons/favicon.svg"
+                  alt="WeKids"
+                  width={65}
+                  height={42}
+                />
+              </div>
+            </div>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
