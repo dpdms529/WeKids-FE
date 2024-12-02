de0e27ff2df06847fb30f9ef8f86c0b2ce2659a0
yoon <cbj0010@naver.com>
2024-12-03 01:23:32 +0900

    [chore/#122] : 문구수정

--- src/ui/components/home/parent/NoAccountCard.jsx
+++ src/ui/components/home/parent/NoAccountCard.jsx
@@ -4,7 +4,7 @@ import { MessageBox } from "../MessageBox";
 const NoAccountCard = ({name}) => {
   return (
     <div className="flex flex-col items-center">
-      <AccountCardLayout title={name + ", 아직 연결 된 계좌가 없네요!"}>
+      <AccountCardLayout title={name + "님, 아직 연결 된 계좌가 없네요!"}>
         <MessageBox
           subtitle={`연결 계좌\n선택하기`}
           description="용돈지급을 위해 우리은행 계좌를 연결해주세요."
