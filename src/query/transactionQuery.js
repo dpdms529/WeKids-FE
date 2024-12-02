98265239d95add8053b1478462bf5540347df91c
jabin1221 <koreanjb1221@gmail.com>
2024-12-03 01:03:44 +0900

    [refact/#106]: query, service 파일 분리

--- src/query/transactionQuery.js
+++ src/query/transactionQuery.js
@@ -1,5 +1,5 @@
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
-import { fetchTransactions } from "../services/transaction";
+import { fetchTransactions, fetchTransactionById, updateTransactionMemo } from "../services/transaction";
 export const useTransactionList = ({
     accountId = 4,
     start,
@@ -7,20 +7,38 @@ export const useTransactionList = ({
     type,
     size = 5,
   }) => {
-    console.log(accountId, start, end, type, size);
     return useInfiniteQuery({
       queryKey: ["transactions", accountId, start, end, type, size],
       queryFn: ({ pageParam = 0 }) => {
         console.log("Fetching page:", pageParam);
         return fetchTransactions({ page: pageParam, start, end, accountId, type, size })
       }
-        
         ,
       getNextPageParam: (lastPage, allPages) => {
-        console.log("Last Page:", lastPage);
-        console.log("HasNext:", lastPage?.hasNext);
-        console.log("Next Page Index:", allPages.length);
         return lastPage?.hasNext ? allPages.length : undefined;
+      },
+    });
+  };
+
+  export const useTransactionDetail = (transactionId) => {
+    return useQuery({
+      queryKey: ["transaction", transactionId],
+      queryFn: () => fetchTransactionById(transactionId),
+      enabled: !!transactionId,
+    });
+  };
+
+  export const useUpdateTransactionMemo = () => {
+    return useMutation({
+      mutationFn: updateTransactionMemo,
+      onMutate: (variables) => {
+        console.log("Mutation started with variables:", variables);
+      },
+      onError: (error, variables, context) => {
+        console.error("Mutation failed:", error, variables, context);
+      },
+      onSuccess: (data, variables, context) => {
+        console.log("Mutation succeeded:", data, variables, context);
       },
     });
   };
--- src/services/transaction.js
+++ src/services/transaction.js
@@ -2,21 +2,21 @@
 import { auth } from "@/auth";
 import { BASE_URL } from "../constants/url";
 
-// export const submitTransfer = async (data) => {
-//   const response = await fetch(`${BASE_URL}/transactions`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   });
-//   if (response.status !== 201) {
-//     throw new Error(`Failed to post data. Status: ${response.status}`);
-//   }
+export const submitTransfer = async (data) => {
+  const response = await fetch(`${BASE_URL}/transactions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (response.status !== 201) {
+    throw new Error(`Failed to post data. Status: ${response.status}`);
+  }
 
-//   const responseBody = await response.text();
-//   return responseBody ? JSON.parse(responseBody) : {};
-// };
+  const responseBody = await response.text();
+  return responseBody ? JSON.parse(responseBody) : {};
+};
 
 export const fetchTransactions = async ({
   page,
@@ -26,10 +26,8 @@ export const fetchTransactions = async ({
   type,
   accountId,
 }) => {
-  console.log("???")
   const session = await auth();
   const authorization = session?.user?.Authorization;
-  console.log("!!!")
   const headers = {
     "Content-Type": "application/json",
     Cookie: `Authorization=${authorization}`,
@@ -68,75 +66,55 @@ export const fetchTransactions = async ({
   }
 };
 
+export const fetchTransactionById = async (transactionId) => {
+  try {
+    if (!transactionId) {
+      throw new Error("Transaction ID is required");
+    }
+
+    const url = `${BASE_URL}/transactions/${transactionId}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Error fetching transaction: ${errorMessage}`);
+    }
+
+    const data = await response.json();
+    console.log("Fetched transaction data:", data);
+    return data;
+  } catch (error) {
+    console.error("Error in fetchTransactionById:", error.message);
+    throw error;
+  }
+};
+
+const updateTransactionMemo = async ({ transactionId, memo }) => {
+  if (!transactionId || memo === undefined || memo.trim() === "") {
+    throw new Error("Transaction ID와 메모는 필수입니다.");
+  }
+
+  const url = `${BASE_URL}/transactions/${transactionId}/memo`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ memo }),
+  });
+  if (response.status !== 204) {
+    const errorBody = await response.text();
+    console.error("Error response:", errorBody);
+    throw new Error(`Error: ${response.status}, ${errorBody}`);
+  }
+};
 
 
-// export const fetchTransactionById = async (transactionId) => {
-//   try {
-//     if (!transactionId) {
-//       throw new Error("Transaction ID is required");
-//     }
-
-//     const url = `${BASE_URL}/transactions/${transactionId}`;
-
-//     const response = await fetch(url, {
-//       method: "GET",
-//     });
-
-//     if (!response.ok) {
-//       const errorMessage = await response.text();
-//       throw new Error(`Error fetching transaction: ${errorMessage}`);
-//     }
-
-//     const data = await response.json();
-//     console.log("Fetched transaction data:", data);
-//     return data;
-//   } catch (error) {
-//     console.error("Error in fetchTransactionById:", error.message);
-//     throw error;
-//   }
-// };
-
-// export const useTransactionDetail = (transactionId) => {
-//   return useQuery({
-//     queryKey: ["transaction", transactionId],
-//     queryFn: () => fetchTransactionById(transactionId),
-//     enabled: !!transactionId,
-//   });
-// };
-
-// const updateTransactionMemo = async ({ transactionId, memo }) => {
-//   if (!transactionId || memo === undefined || memo.trim() === "") {
-//     throw new Error("Transaction ID와 메모는 필수입니다.");
-//   }
-
-//   const url = `${BASE_URL}/transactions/${transactionId}/memo`;
-
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ memo }),
-//   });
-//   if (response.status !== 204) {
-//     const errorBody = await response.text();
-//     console.error("Error response:", errorBody);
-//     throw new Error(`Error: ${response.status}, ${errorBody}`);
-//   }
-// };
-
-// export const useUpdateTransactionMemo = () => {
-//   console.log("Initializing useUpdateTransactionMemo");
-//   return useMutation({
-//     mutationFn: updateTransactionMemo,
-//     onMutate: (variables) => {
-//       console.log("Mutation started with variables:", variables);
-//     },
-//     onError: (error, variables, context) => {
-//       console.error("Mutation failed:", error, variables, context);
-//     },
-//     onSuccess: (data, variables, context) => {
-//       console.log("Mutation succeeded:", data, variables, context);
-//     },
-//   });
-// };
