231a3b39131e815f4110dadbef8bce94d2314ce4
jabin1221 <koreanjb1221@gmail.com>
2024-12-04 10:00:39 +0900

    [refact/#106]: 주석 해제

--- src/apis/design.js
+++ src/apis/design.js
@@ -4,28 +4,28 @@ import { auth } from "@/auth";
 import { BASE_URL } from "../constants/url";
 
 // 디자인 생성
-// export const designCreate = async (data) => {
-//   const session = await auth();
-//   const authorization = session?.user?.Authorization;
+export const designCreate = async (data) => {
+  const session = await auth();
+  const authorization = session?.user?.Authorization;
 
-//   const headers = {
-//     "Content-Type": "application/json",
-//     Cookie: `Authorization=${authorization}`,
-//   };
+  const headers = {
+    "Content-Type": "application/json",
+    Cookie: `Authorization=${authorization}`,
+  };
 
-//   const response = await fetch(`${BASE_URL}/design`, {
-//     method: "POST",
-//     headers: headers,
-//     body: JSON.stringify(data),
-//   });
-//   console.log(response);
-//   if (!response.ok) {
-//     throw new Error(`Failed to post data: ${response.statusText}`);
-//   }
+  const response = await fetch(`${BASE_URL}/design`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+  console.log(response);
+  if (!response.ok) {
+    throw new Error(`Failed to post data: ${response.statusText}`);
+  }
 
-//   const responseBody = await response.body();
-//   return responseBody ? JSON.parse(responseBody) : {};
-// };
+  const responseBody = await response.body();
+  return responseBody ? JSON.parse(responseBody) : {};
+};
 
 // 디자인 조회
 export const designFetch = async () => {
