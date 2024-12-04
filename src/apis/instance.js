47548d267ca3c93257818bd73c79247dbc73d70c
yoon <cbj0010@naver.com>
2024-12-04 10:31:33 +0900

    [feat/#105] : 공통 api 분리

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,12 +1,12 @@
-import { auth } from '@/auth';
-import { BASE_URL } from '@/constants/auth';
+import { auth } from "@/auth";
+import { BASE_URL } from "../constants/url";
 
 const fetchInstance = async (url, options) => {
   const session = await auth();
   const accessToken = session?.accessToken;
 
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     ...options.headers,
   };
 
@@ -23,30 +23,32 @@ const fetchInstance = async (url, options) => {
     if (!response.ok) {
       const errorResponse = await response.json();
       if (response.status === 403) {
-        console.error('Token Expired');
+        console.error("Token Expired");
       }
-      console.error('Fetch Error:', errorResponse);
+      console.error("Fetch Error:", errorResponse);
       return errorResponse;
     } else {
       // 로그인/로그아웃의 경우 response.headers에서 토큰 추출
-      if (url == 'members/login' || url == 'members/logout') return response;
+      if (url == "members/login" || url == "members/logout") return response;
 
       // 그 외의 경우
-      if (response.headers.get('Content-Type')?.includes('application/json')) return await response.json();
+      if (response.headers.get("Content-Type")?.includes("application/json"))
+        return await response.json();
       else return await response.text();
     }
   } catch (error) {
-    console.log('Fetch error: ', error);
+    console.log("Fetch error: ", error);
     throw new Error(`Fetch error [${error}]`);
   }
 };
 
 const instance = {
-  get: (url, options) => fetchInstance(url, { ...options, method: 'GET' }),
-  post: (url, options) => fetchInstance(url, { ...options, method: 'POST' }),
-  put: (url, options) => fetchInstance(url, { ...options, method: 'PUT' }),
-  patch: (url, options) => fetchInstance(url, { ...options, method: 'PATCH' }),
-  delete: (url, options) => fetchInstance(url, { ...options, method: 'DELETE' }),
+  get: (url, options) => fetchInstance(url, { ...options, method: "GET" }),
+  post: (url, options) => fetchInstance(url, { ...options, method: "POST" }),
+  put: (url, options) => fetchInstance(url, { ...options, method: "PUT" }),
+  patch: (url, options) => fetchInstance(url, { ...options, method: "PATCH" }),
+  delete: (url, options) =>
+    fetchInstance(url, { ...options, method: "DELETE" }),
 };
 
 export default instance;
