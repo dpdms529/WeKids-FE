60208159872257c984b41191c6382c6152afd6fd
jabin1221 <koreanjb1221@gmail.com>
2024-12-05 18:33:08 +0900

    [feat/#136]: s3 연동한 미션 인증 api 연결 + not found 페이지

--- src/apis/mission.js
+++ src/apis/mission.js
@@ -84,26 +84,34 @@ export const showMissionList = async ({state, category, child}) => {
     return data;
   };
 
-  export const missionAuth = async ({missionId, memo, image}) => {
+  export const missionAuth = async ({ missionId, memo, image }) => {
     const session = await auth();
     const authorization = session?.user?.Authorization;
-    const headers = {
-      "Content-Type": "application/json",
-      Cookie: `Authorization=${authorization}`,
-    };
-    const response = await fetch(
-      `${BASE_URL}/missions/${missionId}/submit`,
-      {
-        method: "PATCH",
-        headers,
-        body: JSON.stringify({
-            data:{
-                memo: memo
-            },
-            image: image,
-          }),
+    console.log(missionId)
+    // FormData 생성
+    const formData = new FormData();
+  
+    // 메모가 있을 경우 추가
+    if (memo) {
+      formData.append(
+        "data",
+        new Blob([JSON.stringify({ memo })], { type: "application/json" })
+      );
+    }
+  
+    // 이미지가 있을 경우 추가
+    if (image) {
+      formData.append("image", image);
+    }
+  
+    const response = await fetch(`${BASE_URL}/missions/${missionId}/submit`, {
+      method: "PATCH",
+      headers: {
+        Cookie: `Authorization=${authorization}`, // Authorization 쿠키
       },
-    );
+      body: formData, // FormData로 전송
+    });
+  
     return response.status !== 204 ? await response.json() : null;
   };
 
