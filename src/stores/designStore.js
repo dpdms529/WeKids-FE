33cc132ef09a1e25d67dc7f0efe2cb21351ad2f7
안찬웅 <1stevering@naver.com>
2024-12-05 10:09:25 +0900

    [file/#126]: 필요 없는 파일 삭제

--- src/stores/designStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useDesignStore = create((set) => ({
-  selectedCharacter: "DADAPING",
-  selectedColor: "BLUE",
-  setSelectedCharacter: (character) => set({ selectedCharacter: character }),
-  setSelectedColor: (color) => set({ selectedColor: color }),
-}));
