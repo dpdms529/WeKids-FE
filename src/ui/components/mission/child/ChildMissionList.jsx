5a30738b8cca772c4cca337dc1681fcde21c9de1
yoon <cbj0010@naver.com>
2024-12-06 12:37:59 +0900

    [design/#144] : padding값 수정

--- src/ui/components/mission/child/ChildMissionList.jsx
+++ src/ui/components/mission/child/ChildMissionList.jsx
@@ -10,22 +10,29 @@ export default function ChildMissionList({ missiondata }) {
       : missionColorMap["NEW"].background;
 
   return (
-    <div className={`w-[322px] rounded-xl ${bgColor} p-5 shadow-md`}>
-      <div className="flex justify-between">
-        <div className="flex-1 space-y-1 ">
-          <h3 className="text-R-14 text-black truncate">{missiondata.title}</h3>
-          <p className="text-R-12 text-sub02 pt-2 break-words">
-            {missiondata.content}
-          </p>
+    <div className="pb-1">
+      <div className={`w-[322px] rounded-xl ${bgColor} p-5 shadow-md`}>
+        <div className="flex justify-between">
+          <div className="flex-1 space-y-1 ">
+            <h3 className="text-R-14 text-black truncate">
+              {missiondata.title}
+            </h3>
+            <p className="text-R-12 text-sub02 pt-2 break-words">
+              {missiondata.content}
+            </p>
+          </div>
+          <div className="flex flex-col items-end gap-2 shrink-0">
+            <StateBadge state={missiondata.state} />
+            <CategoryBadge
+              missionType={missiondata.category}
+              isButton={false}
+            />
+          </div>
         </div>
-        <div className="flex flex-col items-end gap-2 shrink-0">
-          <StateBadge state={missiondata.state} />
-          <CategoryBadge missionType={missiondata.category} isButton={false} />
-        </div>
+        <p className="text-R-10 text-sub02/60 text-right mt-3">
+          🍪 {formatDate(missiondata.deadline)}
+        </p>
       </div>
-      <p className="text-R-10 text-sub02/60 text-right mt-3">
-        🍪 {formatDate(missiondata.deadline)}
-      </p>
     </div>
   );
 }
